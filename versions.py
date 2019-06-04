15a0514d1b7f94150f65cc4b61da014d02963c72
shinjh0849 <shinjh0849@gmail.com>
2019-06-05 00:37:24 +0900

    in mac

--- versions.py
+++ versions.py
@@ -21,7 +21,7 @@ import theano
 print('theano: %s' % theano.__version__)
 # tensorflow
 import tensorflow
-print('tensorflow: %s' % tensorflow.__version__)
+print('tensorflow: %s' % tensorfflow.__version__)
 # keras
 import keras
 print('keras: %s' % keras.__version__)
